a165ad4dcfab125de12945b9f09d9735f44e1dac
Ismail Temuroglu <ismailtemuroglu2819@gmail.com>
2026-02-07 23:27:01 +0300

    feat: added budgetLimitExceeded event for false budget operations

---
+++ Wealthra.Domain/Events/BudgetLimitExceededEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Wealthra.Domain.Common;
+using Wealthra.Domain.Entities;
+
+namespace Wealthra.Domain.Events
+{
+    public class BudgetLimitExceededEvent : IDomainEvent
+    {
+        public BudgetLimitExceededEvent(Budget budget, decimal expenseAmount)
+        {
+            Budget = budget;
+            AttemptedExpenseAmount = expenseAmount;
+            OccurredOn = DateTime.UtcNow;
+        }
+
+        public Budget Budget { get; }
+        public decimal AttemptedExpenseAmount { get; }
+        public DateTime OccurredOn { get; }
+    }
+}
