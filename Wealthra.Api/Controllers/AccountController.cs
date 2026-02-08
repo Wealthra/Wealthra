2dc704c0b21acd6ea442396682694d99ca4e9ea5
Ismail Temuroglu <ismailtemuroglu2819@gmail.com>
2026-02-08 21:01:13 +0300

    feat: add controllers

---
+++ Wealthra.Api/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Wealthra.Application.Features.Identity.Commands.Login;
+
+namespace Wealthra.Api.Controllers
+{
+    public class AccountController : ApiControllerBase
+    {
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(LoginUserCommand command)
+        {
+            // Mediator handles Validation -> Logic -> Response
+            var token = await Mediator.Send(command);
+            return Ok(new { Token = token });
+        }
+
+    }
+}
---
+++ Wealthra.Api/Controllers/ExpenseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Wealthra.Application.Features.Expenses.Commands.CreateExpense;
+
+namespace Wealthra.Api.Controllers
+{
+    [Authorize] // Requires JWT
+    public class ExpensesController : ApiControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<int>> Create(CreateExpenseCommand command)
+        {
+            var id = await Mediator.Send(command);
+            return Ok(id);
+        }
+    }
+}
